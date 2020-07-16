f2d8c643946b091e632e9ef79386d97f120961b0
Joel Stockamp <stockampj@gmail.com>
2020-07-16 16:59:38 -0700

    add id to rows

--- trPoint_FlashDrive_SetUp.js
+++ trPoint_FlashDrive_SetUp.js
@@ -10,26 +10,27 @@ function selectAllCheckboxes() {
 // This snippet will gather all rows with docnames and format them into an Object//////////////////////////////
 
 
-
 let documentList={};
 let rowId = 0;
 Object.keys($("tr")).forEach((key)=>{
   let row = $("tr")[key];
   if (row.children && row.children.length === 12 && !(row.classList.contains('tr-head'))){
-    let docCheckBoxId= row.children[0].children[0].children[0].id;
+    row.id= `docRowId-${rowId}`
+    
     let docName = row.children[2].children[0].outerText;
     let docCategory = row.children[2].children[1].outerText;
+    let docCheckBoxId= row.children[0].children[0].children[0].id;
     let docLinkId = row.children[2].children[0].children[0].id;
 
     let tempDocRows = documentList;    
     let rowObject = Object.assign({}, tempDocRows, {
       [rowId]: {
-        rowId: rowId,
-        rowNode: row,
-        docCheckBoxId: docCheckBoxId,
-        docLinkId: docLinkId,
+        docRowId: row.id,
         docName: docName,
-        docCategory: docCategory
+        docCategory: docCategory,
+        docNameCategoryCombined: `${docName.toLowerCase()} ${docCategory.toLowerCase()}`,
+        docCheckBoxId: docCheckBoxId,
+        docLinkId: docLinkId
       }
     })
     documentList = rowObject;
@@ -39,10 +40,3 @@ Object.keys($("tr")).forEach((key)=>{
 console.log(documentList);
 
 
-
-
-
-
-<a id="ctl00_ctl00_ctl00_cphTPMainBodyContent_cphTCBodyContent_cphTabContent_dgTxnDocList_ctl04_lbViewDocument" title="Agency From Listing Agent to Seller" href="javascript:WebForm_DoPostBackWithOptions(new WebForm_PostBackOptions(&quot;ctl00$ctl00$ctl00$cphTPMainBodyContent$cphTCBodyContent$cphTabContent$dgTxnDocList$ctl04$lbViewDocument&quot;, &quot;&quot;, true, &quot;&quot;, &quot;&quot;, false, true))">Agency From Listing Agent to Seller</a>
-
-<div class="small" style="color:GrayText; font-style:italic;">(Agency From Listing Agent to Seller)</div>
