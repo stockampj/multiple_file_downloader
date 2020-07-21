cf2f4b7978328c35468e566a7ed970dd3ad538ce
Joel Stockamp <stockampj@gmail.com>
2020-07-21 15:19:57 -0700

    add search filter to puruse doclist

--- trPoint_FlashDrive_SetUp.js
+++ trPoint_FlashDrive_SetUp.js
@@ -9,34 +9,63 @@ function selectAllCheckboxes() {
 
 // This snippet will gather all rows with docnames and format them into an Object//////////////////////////////
 
+function gatherDocumentList() {
+  let documentList = {};
+  let rowId = 0;
+  Object.keys($("tr")).forEach((key)=>{
+    let row = $("tr")[key];
+    if (row.children && row.children.length === 12 && !(row.classList.contains('tr-head'))){
+      row.id = `docRowId-${rowId}`
+      
+      let docName = row.children[2].children[0].outerText;
+      let docCategory = row.children[2].children[1].outerText;
+      let docCheckBoxId= row.children[0].children[0].children[0].id;
+      let docLinkId = row.children[2].children[0].children[0].id;
+      let pageCount = row.children[3].innerText;
+      let uploadDate = row.children[6].children[0].innerText;
+  
+      let tempDocRows = documentList;    
+      let rowObject = Object.assign({}, tempDocRows, {
+        [rowId]: {
+          docRowId: row.id,
+          docName: docName,
+          docCategory: docCategory,
+          docNameCategoryCombined: `${docName.toLowerCase()} ${docCategory.toLowerCase()}`,
+          docCheckBoxId: docCheckBoxId,
+          docLinkId: docLinkId,
+          pageCount: pageCount,
+          uploadDate: uploadDate
+        }
+      })
+      documentList = rowObject;
+      rowId+=1;
+    };
+  })
+  return documentList;
+}
 
-let documentList={};
-let rowId = 0;
-Object.keys($("tr")).forEach((key)=>{
-  let row = $("tr")[key];
-  if (row.children && row.children.length === 12 && !(row.classList.contains('tr-head'))){
-    row.id= `docRowId-${rowId}`
-    
-    let docName = row.children[2].children[0].outerText;
-    let docCategory = row.children[2].children[1].outerText;
-    let docCheckBoxId= row.children[0].children[0].children[0].id;
-    let docLinkId = row.children[2].children[0].children[0].id;
 
-    let tempDocRows = documentList;    
-    let rowObject = Object.assign({}, tempDocRows, {
-      [rowId]: {
-        docRowId: row.id,
-        docName: docName,
-        docCategory: docCategory,
-        docNameCategoryCombined: `${docName.toLowerCase()} ${docCategory.toLowerCase()}`,
-        docCheckBoxId: docCheckBoxId,
-        docLinkId: docLinkId
-      }
+
+function findDocument(documentList, searchTerm, property) {
+  function termCleaner(string){
+    return string.toLowerCase().replace(/[()]/gmi, "")
+  }
+  let keyResult = [];
+  let term = termCleaner(searchTerm);
+  let searchField= property;
+  console.log(searchField)
+  Object.keys(documentList).forEach((key)=>{
+    let match = false;
+    let comparisonField = termCleaner(documentList[key][searchField]);
+    term.split(' ').forEach(word =>{
+      if (comparisonField.includes(word.trim())) {match = true;}
     })
-    documentList = rowObject;
-    rowId+=1;
-  };
-})
-console.log(documentList);
+    if (match) {keyResult.push(key)};
+  })
+  return keyResult;
+}
 
-
+selectAllCheckboxes();
+let masterDocumentList = gatherDocumentList();
+let matches = findDocument(masterDocumentList, "agency", "docNameCategoryCombined");
+console.log(matches)
